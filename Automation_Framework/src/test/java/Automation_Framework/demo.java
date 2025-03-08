103d42f4f490e2fd899e1752e45a434790f53197
Aish97Riya <aiswaryaks97@outlook.com>
2025-03-08 21:38:48 +0530

    my first commit

---
+++ Automation_Framework/src/test/java/Automation_Framework/demo.java
@@ -0,0 +1,16 @@
+package Automation_Framework;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test()
+	public static void add()
+	{
+		int a=10; 
+		int b=21;
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
