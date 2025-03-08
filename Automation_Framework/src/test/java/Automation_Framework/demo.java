64419fc81872919857381ba787d2b9962d7d6e36
Aish97Riya <aiswaryaks97@outlook.com>
2025-03-08 21:48:26 +0530

    story1changes

---
+++ Automation_Framework/src/test/java/Automation_Framework/demo.java
@@ -0,0 +1,17 @@
+package Automation_Framework;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test()
+	public static void add()
+	{
+		int a=10; 
+		int b=21;
+		int c=a+b;
+		System.out.println(c);
+		System.out.println("Udhay");
+		}
+
+}
